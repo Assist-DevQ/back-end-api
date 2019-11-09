2f6640ef163a6dcb67196be2c85583233794a455
Petru Cioata <petru.cioata@assist.ro>
2019-11-09 19:33:48 +0200

    Update type for data and time

--- db/migrate/20191105153939_create_events.rb
+++ db/migrate/20191105153939_create_events.rb
@@ -2,8 +2,8 @@ class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
       t.string :name
-      t.text :data
-      t.string :time
+      t.json :data
+      t.bigint :time
       t.references :scenario, null: false, foreign_key: true
 
       t.timestamps
