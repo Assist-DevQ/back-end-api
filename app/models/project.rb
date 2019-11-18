8bbca2fc19ac7de3004f772ef0d74649699f2686
nicoleta <nicoleta.cajvan@gmail.com>
2019-11-18 23:40:55 +0200

    Added before_save callback for user and repo in project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,13 @@
 class Project < ApplicationRecord
   default_scope -> { where(deleted: false) }
 
+  before_save :generate_user_and_repo
+
   has_many :scenarios
+
+  def generate_user_and_repo
+    repo_splited = repository_link.split('/')
+    self.user = repo_splited[-2]
+    self.repository_name = repo_splited[-1]
+  end
 end
