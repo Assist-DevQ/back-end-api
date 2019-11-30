2e6e90528b70707a3d85e06b8f9ae5e9de0bb98e
Petru Cioata <petru.cioata@assist.ro>
2019-11-30 16:25:03 +0200

    Set master branch when a new project is saved

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,13 +2,18 @@ class Project < ApplicationRecord
   default_scope -> { where(deleted: false) }
 
   before_save :extract_github_user_and_repo
+  after_save :set_base_branch
 
-  has_many :scenarios
-  has_many :branches
+  has_many :scenarios, dependent: :destroy
+  has_many :branches, dependent: :destroy
 
   private
 
   def extract_github_user_and_repo
     self.user_repo, self.repository_name = repository_link.split('/').last(2)
+  end
+
+  def set_base_branch
+    BranchManager.new(self, 'master').call
   end
 end
