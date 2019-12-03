57f4b8df710454813b55743b5da1622a9b1c698f
Petru Cioata <petru.cioata@assist.ro>
2019-12-03 19:41:17 +0200

    Save short hash commit for branch

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,5 +1,13 @@
 class Branch < ApplicationRecord
   belongs_to :project
 
+  before_save :short_hash_commit
+
   validates :name, uniqueness: { scope: :project_id }
+
+  private
+
+  def short_hash_commit
+    self.current_hash = current_hash[0..6]
+  end
 end
