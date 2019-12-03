1baeffd0e986bf68157babb0b836b9bf2f4c7b8f
Petru Cioata <petru.cioata@assist.ro>
2019-12-03 21:46:53 +0200

    Use text instead of string for array columns

--- db/migrate/20191203171954_add_has_diff_array_to_runs.rb
+++ db/migrate/20191203171954_add_has_diff_array_to_runs.rb
@@ -1,5 +1,5 @@
 class AddHasDiffArrayToRuns < ActiveRecord::Migration[6.0]
   def change
-    add_column :runs, :has_diff, :string, default: [].to_yaml
+    add_column :runs, :has_diff, :text
   end
 end
